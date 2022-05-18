36f5ace9f4fe33ca12971f65ee3f4507a3798f4a
Matt Montgomery <mmontgomery@deseretdigital.com>
2022-05-18 14:42:54 -0600

    feat: ✨ adds pr status tool

--- list-prs.sh
+++ list-prs.sh
@@ -1,4 +1,6 @@
-gh pr list -R mattmontgomery/dotutils
-gh pr list -R mattmontgomery/formguide
-gh pr list -R mattmontgomery/matthew-montgomery.com
-gh pr list -R mattmontgomery/lineup-graphic-builder
+PAGER_STATE=$(gh config get pager)
+gh config set pager "less -X -F"
+
+while read -r line; do gh pr list -R $line; done < .repos
+
+gh config set pager $PAGER_STATE
